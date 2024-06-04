a6b907237ae6c911c0e19dc921868e9a57c1d250
Sgale <97342601+Sgale952@users.noreply.github.com>
2024-06-04 09:27:18 +0700

    correct column names

--- users/security.sql
+++ users/security.sql
@@ -3,8 +3,8 @@ CREATE TYPE user_role AS ENUM ('USER', 'AUTHOR', 'ADMIN');
 CREATE TABLE users.security(
     item_id INT NOT NULL UNIQUE,
     email VARCHAR(30) NOT NULL UNIQUE,
-    passwrd VARCHAR(64) NOT NULL,
-    blocked BOOLEAN NOT NULL DEFAULT false,
+    password VARCHAR(64) NOT NULL,
+    access BOOLEAN NOT NULL DEFAULT true,
     access_role user_role NOT NULL DEFAULT 'USER'
 
     FOREIGN KEY (item_id) REFERENCES users.profile(item_id) ON DELETE CASCADE
